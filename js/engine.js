476c7fb39989685bf6d469ea29f05598f4e1dcbd
Ottilie Williams <ottiliewilliams1@sky.com>
2018-11-20 13:50:37 +0000

    Got it to work again

--- js/engine.js
+++ js/engine.js
@@ -2,58 +2,34 @@
 
 var choices = ["rock", "paper", "scissors"];
 
+var play_machine;
+var play_you;
 var played = 0;
 var won = 0;
 var lost = 0;
 var draw = 0;
 
-$('button').click(function(event){  var play_you = this.getAttribute("value");  console.log(play_you);
+/* Listen for the button clicks */
+
+$('button').click(function(event){
+  var play_you = this.getAttribute("value");
 $("#play_you").text(play_you);
 var play_machine = computer_move();
 compare(play_you, play_machine);
 });
-
-/* Listen for the button clicks */
-
-/*
-$("#rock").click(function(event) {
-  var move_you = 'rock';
-
-});
-
-$("#paper").click(function(event) {
-  var move_you = 'paper';
-  $("#play_you").text(move_you);
-  var play_machine = computer_move();
-  compare(move_you, play_machine);
-});
-
-$("#scissors").click(function(event) {
-  var move_you = 'scissors';
-  $("#play_you").text(move_you);
-  var play_machine = computer_move();
-  compare(move_you , play_machine);
-});
-*/
 
 function computer_move() {
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
 
   play_machine = choices[play_machine];
   $("#play_machine").text(play_machine);
-  console.log('move: ' + play_machine);
-
-$("#game_won").empty();
-$("#game_lost").empty();
-$("#game_draw").empty();
-
   return play_machine;
 }
 
 
 function compare(you, machine){
-
   var verdict = 'lose';
+
 
   if ( you == machine ) {
     verdict = 'draw';
@@ -71,6 +47,7 @@ function compare(you, machine){
     verdict = 'win';
   }
 
+/*
   if ( verdict == 'win') {
     won++;
     $("#game_won").text(won);
@@ -84,26 +61,23 @@ function compare(you, machine){
     draw++;
     $("#game_draw").text(draw);
   }
+  */
+
+switch (verdict) {
+    case 'win':won++;
+    $("#game_won").text(won);
+    break;
+    case 'lose':
+    lost++;
+    $("#game_lost").text(lost);
+    break;  case 'draw':
+    draw++;
+    $("#game_draw").text(draw);
+    break;
+  }
 
   $("#verdict").text(verdict);
 
   played++;
   $("#game_played").text(played);
   }
-
-
-
-/*
-switch (verdict) {
-  case 'win': won++;
-  $("#won").text(won);
-  break;
-  case 'lose':
-  lost++;
-  $("#lost").text(lost);
-  break;
-  case 'draw':
-  draw++;
-  $("#draw").text(draw);
-  break;}
-*/
